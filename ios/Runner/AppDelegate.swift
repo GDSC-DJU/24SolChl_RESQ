773222347ad82b6c5135a075814494f6d9add423
애리 <aeri123443@gmail.com>
2024-01-29 15:01:41 +0900

    **Feat** : Add notification
    로컬 푸시 알림 기능 추가
    조건 설정 -  변수 모니터링하여 알림이 뜨도록 함

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,9 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    if #available(iOS 10.0, *){ // iOS 10 이상은 User Notifications 프레임워크로 푸시 알림 처리
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
